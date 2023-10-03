436909d84a3298528ef90bad340070a67f1de96b
tudorcrisan1231 <crisantudor79@yahoo.com>
2023-10-04 01:12:25 +0300

    docker setup for php + mysql

---
+++ Docker/entrypoint.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+# if vendor folder does not exist, run composer install
+if [ ! -f "vendor/autoload.php" ]; then
+    composer install --no-progress --no-interaction
+fi
+
+
+#if .env file does not exist, copy .env.example to .env
+if [ ! -f ".env" ]; then
+    echo "Creating env file for env $APP_ENV"
+    cp .env.example .env
+else
+    echo "Env file exists"
+fi
+
+
+php artisan migrate
+php artisan key:generate
+# php artisan cache:clear
+# php artisan config:clear
+# php artisan route:clear
+
+php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
+exec docker-php-entrypoint "$@"
--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,14 @@
-    
+FROM php:8.1 as php
+
+RUN apt-get update -y
+#extension for php
+RUN apt-get install -y unzip libpq-dev libcurl4-gnutls-dev 
+RUN docker-php-ext-install pdo pdo_mysql bcmath
+
+WORKDIR /var/www
+COPY . .
+
+COPY --from=composer:2.5.8 /usr/bin/composer /usr/bin/composer
+
+ENV PORT=8000
+ENTRYPOINT [ "Docker/entrypoint.sh" ]
