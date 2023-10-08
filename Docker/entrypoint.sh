00193675eaab94072d38a743bf68ffd2db0de4dc
tudorcrisan1231 <crisantudor79@yahoo.com>
2023-10-08 03:16:51 +0300

    change docker commnds order

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,12 +1,5 @@
 #! /bin/bash
 
-# if vendor folder does not exist, run composer install
-if [ ! -f "vendor/autoload.php" ]; then
-    composer install --no-progress --no-interaction
-    php artisan db:seed
-fi
-
-
 #if .env file does not exist, copy .env.example to .env
 if [ ! -f ".env" ]; then
     echo "Creating env file for env $APP_ENV"
@@ -15,13 +8,21 @@ else
     echo "Env file exists"
 fi
 
+# if vendor folder does not exist, run composer install
+if [ ! -f "vendor/autoload.php" ]; then
+    composer install --no-progress --no-interaction
+    php artisan db:seed
+fi
+
+
 
 php artisan migrate
 php artisan storage:link
 php artisan key:generate
-# php artisan cache:clear
-# php artisan config:clear
-# php artisan route:clear
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
 
 php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
 exec docker-php-entrypoint "$@"
